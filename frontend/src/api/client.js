2634b5d924e6a4d5a28954a6f3e9d81109cdead4
karrthik08 <rc62373@umbc.edu>
2026-04-18 10:10:03 -0400

    backend URL

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL + '/api/v1',
+  baseURL: 'https://hope-backend-yc30.onrender.com/api/v1',
   headers: { 'Content-Type': 'application/json' },
 })
 
